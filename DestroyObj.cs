9b456e886f4300804b0b33430385b99e7c17e2dd
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-08-18 16:07:50 +0900

    08 18 유니티 치킨게임

---
+++ DestroyObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour
+{
+    public float deleteTIme = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deleteTIme);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
