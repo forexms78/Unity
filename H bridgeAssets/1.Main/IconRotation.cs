63a341b38b15998abd683b5802515ba05731e59b
forexms78 <49295076+forexms78@users.noreply.github.com>
2020-09-15 17:42:50 +0900

    Add files via upload

---
+++ H bridgeAssets/1.Main/IconRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconRotation : MonoBehaviour
+{
+    public float turnSpeed = 500;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
---
+++ H bridgeAssets/1.Main/texttoggle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class texttoggle : MonoBehaviour
+{
+
+    public float interval = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Toggle();
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("SecondScenes");
+        }
+    }
+
+    void Toggle()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+        Invoke("Toggle", interval);
+    }
+}
